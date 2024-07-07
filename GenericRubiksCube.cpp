bc2e2ba66b315458ffe3fb37bac14d5630d3c6a0
Vedant Agrawal <vedantagrawal1709@gmail.com>
2024-07-07 22:52:05 +0530

    Added GenericRubiksCube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LENOVO on 07-07-2024.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by LENOVO on 07-07-2024.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
