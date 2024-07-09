8747e7250e12f6432063268bd9c3232f6c32658a
Vedant Agrawal <vedantagrawal1709@gmail.com>
2024-07-09 20:52:57 +0530

    Added Solver files

--- Solver/IDDFSSolver.h
+++ Solver/IDDFSSolver.h
@@ -18,7 +18,7 @@ private:
 public:
     T rubiksCube;
 
-    IDDFSSolver(T _rubiksCube. int _max_search_depth = 7) {
+    IDDFSSolver(T _rubiksCube, int _max_search_depth = 7) {
         rubiksCube = _rubiksCube;
         max_search_depth = _max_search_depth;
     }
